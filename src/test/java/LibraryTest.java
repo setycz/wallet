1ef12bdaf3f1e481170a48158f8a6df496040508
SetyCz <setycz@gmail.com>
2017-01-29 17:24:00 +0100

    init

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,8 @@
+public class LibraryTest {
+	/*
+    @Test public void testSomeLibraryMethod() {
+        com.setycz.wallet.Main classUnderTest = new com.setycz.wallet.Main();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+	*/
+}
